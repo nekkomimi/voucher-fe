101b354c6ebce6113f8cd56b1c667496f502c7ce
Ariel Ashari <arielashari00@gmail.com>
2023-09-19 01:35:22 +0700

    fix: responsive landing

--- app/page.tsx
+++ app/page.tsx
@@ -113,45 +113,74 @@ export default function Home() {
   }
 
   return (
-    <div className='w-screen h-screen bg-black justify-center items-center mx-auto'>
-
-      {/* Carousel */}
-      <div className='w-screen'>
+    <div className='mx-auto w-screen h-screen bg-black relative'>
+      <div className='max-w mx-auto'>
         <Carousel autoplay>
           {new Array(3).fill(1).map((it, idx) => {
             return (
-                <Image src={BannerSlider} alt='banner' className='h-[425px] sm:h-[300px] lg:h-[300px] xl:h-[225px]'></Image>
+              <Image src={BannerSlider} alt='Banner' className='h-[250px] lg:h-[425px]'></Image>
             )
           })}
         </Carousel>
       </div>
-      <div className='text-4xl text-white mt-1 flex items-center justify-center'>Choose Your Plan</div>
-      <div className='text-center'>
-        <div className='flex flex-col md:flex-row justify-center items-center gap-x-6 sm:gap-x-12 lg:gap-x-16'>
-          <Image src={GoldButton} alt='gold' className='w-[454px] h-[244px] xl:h-[150px] xl:w-[270px]' onClick={() => {
-            setVoucherType("1")
-            handleModalOpen();
-          }}></Image>
-          <Image src={DiamondButton} alt='diamond' className='w-[454px] h-[244px] xl:h-[150px] xl:w-[270px]' onClick={() => {
-            setVoucherType("2")
-            handleModalOpen();
-          }}></Image>
-        </div>
+      <div className='text-2xl lg:text-4xl text-white mt-1 flex items-center justify-center'>Choose Your Plan</div>
+      <div className='flex flex-col md:flex-row justify-center items-center gap-x-6 sm:gap-x-12 lg:gap-x-16'>
+        <Image src={GoldButton} alt='GoldButton' className='h-[200px] w-[350px] lg:h-[244px] lg:w-[454px]' onClick={() => {
+          setVoucherType("1")
+          handleModalOpen()
+        }}></Image>
+        <Image src={DiamondButton} alt='DiamondButton' className='h-[200px] w-[350px] lg:h-[244px] lg:w-[454px]'  onClick={() => {
+          setVoucherType("2")
+          handleModalOpen()
+        }}></Image>
       </div>
-
-      {/* Posters Section */}
-      <div className='z-40 space-y-5 sm:space-y-10 text-center mt-5'>
-        <div className='flex flex-col md:flex-row justify-center items-center gap-x-5 sm:gap-x-10 lg:gap-x-16 gap-y-3'>
-          {new Array(5).fill(1).map((it, idx) => {
-            return (<>
-              <Image src={Poster} alt='Poster' className='w-[200px] h-[300px] xl:w-[130px] xl:h-[200px]'></Image>
-            </>)
-          })}
-        </div>
+      <div className='flex flex-col md:flex-row justify-center items-center gap-x-5 sm:gap-x-10 lg:gap-x-16 gap-y-3 mt-2'>
+        {new Array(5).fill(1).map((it, idx) => {
+          return (
+            <Image src={Poster} alt='Poster' className='w-[100px] h-[150px] lg:w-[200px] lg:h-[300px]' ></Image>
+          )
+        })}
       </div>
       <FormModal {...formModalProps}></FormModal>
       <SuccessModal {...successModalProps}></SuccessModal>
     </div>
+    // <div className='w-screen h-screen bg-black justify-center items-center mx-auto'>
+
+    //   {/* Carousel */}
+    //   <div className='w-screen'>
+    //     <Carousel autoplay>
+    //       {new Array(3).fill(1).map((it, idx) => {
+    //         return (
+    //             <Image src={BannerSlider} alt='banner' className='h-[425px] sm:h-[300px] lg:h-[300px] xl:h-[225px]'></Image>
+    //         )
+    //       })}
+    //     </Carousel>
+    //   </div>
+    //   <div className='text-4xl text-white mt-1 flex items-center justify-center'>Choose Your Plan</div>
+    //   <div className='text-center'>
+    //     <div className='flex flex-col md:flex-row justify-center items-center gap-x-6 sm:gap-x-12 lg:gap-x-16'>
+    //       <Image src={GoldButton} alt='gold' className='w-[454px] h-[244px] xl:h-[150px] xl:w-[270px]' onClick={() => {
+    //         setVoucherType("1")
+    //         handleModalOpen();
+    //       }}></Image>
+    //       <Image src={DiamondButton} alt='diamond' className='w-[454px] h-[244px] xl:h-[150px] xl:w-[270px]' onClick={() => {
+    //         setVoucherType("2")
+    //         handleModalOpen();
+    //       }}></Image>
+    //     </div>
+    //   </div>
+
+    //   {/* Posters Section */}
+    //   <div className='z-40 space-y-5 sm:space-y-10 text-center mt-5'>
+    //     <div className='flex flex-col md:flex-row justify-center items-center gap-x-5 sm:gap-x-10 lg:gap-x-16 gap-y-3'>
+    //       {new Array(5).fill(1).map((it, idx) => {
+    //         return (<>
+    //           <Image src={Poster} alt='Poster' className='w-[200px] h-[300px] xl:w-[130px] xl:h-[200px]'></Image>
+    //         </>)
+    //       })}
+    //     </div>
+    //   </div>
+    // </div>
   )
 }
 
